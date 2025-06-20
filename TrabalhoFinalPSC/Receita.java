5c4a0d005697ab3fc38069dc1eca72ed4fbdd185
Gabrielly Ferreira <155073067+gabs-fe@users.noreply.github.com>
2025-06-20 17:05:36 -0300

    Update Receita.java

--- TrabalhoFinalPSC/Receita.java
+++ TrabalhoFinalPSC/Receita.java
@@ -1,11 +1,11 @@
 package TrabalhoFinalPSC;
 
-public class Receita extends Transacao { // extends é herança (esta herdando a classe transacao)
+public class Receita extends Transacao { 
   public Receita(double valor,String descricao) {
-    super(valor, descricao); // chama o construtor da classe mãe (Transacao) e enviar os dados para a mesma.
+    super(valor, descricao); 
   }  
-  @Override // avisando que vc esta substituindo um metodo herdado (getTipo)
-  public String getTipo() { // identificou o metodo herdado
-    return "Receita"; // atribuiu o tipo
+  @Override 
+  public String getTipo() {
+    return "Receita"; 
   }
 }
